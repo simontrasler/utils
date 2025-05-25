2cf8282c854a00db541273350909b40a165ecb0b
Simon Trasler <simontrasler@gmail.com>
2025-05-25 10:49:49 -0700

    Support for converting this registry for use with proto deserialization

--- src/main/java/org/trasler/utils/openrtb/ExtensionRegistry.java
+++ src/main/java/org/trasler/utils/openrtb/ExtensionRegistry.java
@@ -26,9 +26,11 @@ package org.trasler.utils.openrtb;
 import com.google.protobuf.Descriptors.Descriptor;
 import com.google.protobuf.Descriptors.FieldDescriptor;
 import com.google.protobuf.Extension;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -37,16 +39,50 @@ import java.util.Set;
  * @author Simon Trasler
  */
 public class ExtensionRegistry {
-    private final Map<String, Set<FieldDescriptor>> extensions = new HashMap<>();
+    private final Map<String, Entry> entries = new HashMap<>();
+
+    // Default entry with immutable collections.
+    private static final Entry EMPTY_ENTRY = new Entry();
 
     public void add(Extension<?, ?> extension) {
         String name = extension.getDescriptor().getContainingType().getFullName();
-        extensions.computeIfAbsent(name, k -> new HashSet<FieldDescriptor>())
-                .add(extension.getDescriptor());
+        entries.computeIfAbsent(name, k -> new Entry())
+                .add(extension);
     }
 
     public Set<FieldDescriptor> getAllExtensions(Descriptor descriptor) {
         String name = descriptor.getFullName();
-        return extensions.getOrDefault(name, Collections.emptySet());
+        return entries.getOrDefault(name, EMPTY_ENTRY).fds;
+    }
+
+    public com.google.protobuf.ExtensionRegistry toGoogleRegistry() {
+        com.google.protobuf.ExtensionRegistry googleRegistry = com.google.protobuf.ExtensionRegistry.newInstance();
+
+        // Copy across all the extensions.
+        entries.forEach((k, v) -> v.extensions.forEach(x -> googleRegistry.add(x)));
+        return googleRegistry;
+    }
+
+    private static class Entry {
+        private List<Extension<?, ?>> extensions = List.of();
+        private Set<FieldDescriptor> fds = Set.of();
+
+        /**
+         * Update the unmodifiable collections for this entry. This operation is
+         * slow to write, so it can be fast and safe to read.
+         *
+         * @param extension The extension to add
+         */
+        public void add(Extension<?, ?> extension) {
+            // Extensions.
+            List<Extension<?, ?>> localExtensions = new ArrayList<>(extensions);
+            localExtensions.add(extension);
+            extensions = Collections.unmodifiableList(localExtensions);
+
+            // Field descriptors.
+            Set<FieldDescriptor> localFds = new HashSet<>(fds);
+            localFds.add(extension.getDescriptor());
+            fds = Collections.unmodifiableSet(localFds);
+        }
     }
 }
